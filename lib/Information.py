85b1522c6fcd8fe429350e02a690c94106c8d222
So6oke <m.soroushtoofan@gmail.com>
2025-08-01 18:58:16 +0330

    Update Information.py

--- lib/Information.py
+++ lib/Information.py
@@ -1,10 +1,10 @@
 from .library import *
 
-admin_user_id = 123456789 #<--- آیدی ادمین
-api_id = 123456789 #<--- آی پی آی آیدی
-api_hash = 'abcdefghigklmnopqrstuvwxyz' #<--- ای پی آی هش
-helper_username = 'BotUserName' #<--- یوزر ربات هلپر بدون @
-bot_token = 'abcdefghigklmnopqrstuvwxyz:466546' #<--- توکن ربات هلپر
+admin_user_id = 7522524886 #<--- آیدی ادمین
+api_id = 28276540 #<--- آی پی آی آیدی
+api_hash = '79256a6149566f43430e2f48833a5322' #<--- ای پی آی هش
+helper_username = 'Controllermmdbot' #<--- یوزر ربات هلپر بدون @
+bot_token = '8321960548:AAHL9qrSxJBi19wzSQzww_hrQ7dDOmeAeLQ' #<--- توکن ربات هلپر
 
 client_id = '01e7dc6b41c3471b94efe87abeb05919'
 client_secret = '4f5f93af1ced4b0d9ba8440606803639'
